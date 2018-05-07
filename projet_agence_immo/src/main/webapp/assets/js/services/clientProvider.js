2e9cfa4b781ff09ab764f960e8796f1a1ec443e3
INTI0325 <INTI0325@DESKTOP-GNJ8QEC>
2018-05-07 09:07:11 +0200

    Marianne : provider Client

--- projet_agence_immo/src/main/webapp/assets/js/services/clientProvider.js
+++ projet_agence_immo/src/main/webapp/assets/js/services/clientProvider.js
@@ -1,62 +1,91 @@
 // Création des services de la classe Standard pour récupérer les données du WebService
-var restURL = "http://localhost:8080/projet_agence_immo/";
-
-angular.module('clientApp').factory(
-		"clientService",
-		function($http) {
-
-			function findOneClient(id, busSC) {
-				$http.get(restURL + "/client?pId=" + id).then(
-						function succesCallback(reponse) {
-							busSC(reponse.data)
-						}, function erreurCallback(reponse) {
-
-							console.log("Erreur----" + reponse.statusText);
-
-						});
-			}
-
-			function findAllClient(busSC) {
-				$http({
-					method : "GET",
-					url : restURL + "/listeCl"
-				}).then(function successCallback(reponse) {
-
-					busSC(reponse.data);
-				}, function erreurCallBack(reponse) {
-
-					console.log("Erreur -------" + reponse.statusText);
-				})
-			}
-
-			function ajoutClient(client, busSC) {
-				$http.post(restURL + "/client").then(
-						function succesCallBack(reponse) {
-							busSC(reponse.data);
-						}, function erreurCallBack(reponse) {
-							console.log("Erreur:" + reponse.statusText)
-						});
-			}
-
-			return {
-				getAllClient : findAllClient,
-				getOneClient : findOneClient,
-				clientAdd : ajoutClient
-			}
-
-		}).factory("adresseService", function($http) {
-	function ajoutAdresse(adresse, busSC) {
-		$http.post(restURL + "/adresse", angular.toJson(adresse), {
-			ContentType : "application/json"
-		}).then(function succes(reponse) {
+	var restURL="http://localhost:8080/projet_agence_immo";
+clientApp.factory("clientService", function($http){
+	
+	function findOneClient(id , busSC) {
+		$http.get(restURL+"/client?pId="+id).then(function succesCallback(reponse) {
+			busSC(reponse.data)
+		} , function erreurCallback(reponse) {
+			
+			console.log("Erreur----"+reponse.statusText);
+			
+		});						
+	}
+	
+	
+	function findAllClient(busSC){
+		$http({
+			method:"GET",
+			url:restURL+"/listeCl"
+		}).then(function successCallback(reponse){
+			
 			busSC(reponse.data);
-		}, function erreur(reponse) {
-			console.log("Erreur --- : " + reponse.statusText)
+		}, function erreurCallBack(reponse){
+			
+			console.log("Erreur -------"+reponse.statusText);
+		})
+	}
+
+	
+	function ajoutClient(client, busSC){
+		$http.post(restURL+"/client", angular.toJson(client), {ContentType:"application/json"}).then(function succesCallBack(reponse){
+			busSC(reponse.data);
+		}, function erreurCallBack(reponse){
+			console.log("Erreur:"+reponse.statusText)
 		});
 	}
 
+	function modifierClient(client, busSC){
+		$http({
+			method:"PUT",
+			url:restURL+"/client",
+			data:angular.toJson(client),
+			headers : {ContentType:"application/json"}
+		}).then(function succesCallback(reponse){
+			busSC(reponse.statusText);
+			
+		}), function erreurCallback(reponse) {
+			console.log("Erreur-------"+reponse.statusText);
+		}
+		
+		
+	}
+	
+	
 	return {
-		addAdresse : ajoutAdresse
+		getAllClient:findAllClient,
+		getOneClient:findOneClient,
+		clientAdd:ajoutClient,
+		modifClient:modifierClient
 	}
 
+	
+}).factory("adresseService", function($http){
+	function ajoutAdresse(adresse, busSC){
+		$http.post(restURL+"/adresse", angular.toJson(adresse), {ContentType:"application/json"}).then(function succes(reponse){
+			busSC(reponse.data);
+		}, function erreur(reponse){
+			console.log("Erreur --- : "+reponse.statusText)
+		});
+	}
+	
+	function modifAdresse(adresse, busSC){
+		$http.put(restURL+"/adresse", angular.toJson(adresse), {ContentType:"application/json"}).then(function succes(reponse){
+			busSC(reponse.data);
+			
+		}, function erreur(reponse){
+			console.log("Erreur------"+reponse.statusText)
+		});
+	}
+	return {
+		addAdresse:ajoutAdresse,
+		updateAdresse:modifAdresse
+	}
+	
+	
 });
+
+
+	
+	
+
